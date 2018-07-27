84dfd79dfdea440f5b5376216bf34798cf21e50e
DDstar <DDstar@gmail.com>
2018-07-27 16:08:03 +0800

    测试库添加工具类 提交测试aar库

---
+++ gihubmavenlibs/src/main/java/com/ddstar/gihubmavenlibs/TestUtil.java
@@ -0,0 +1,14 @@
+package com.ddstar.gihubmavenlibs;
+
+import android.util.Log;
+
+/**
+ * Created by DDStar on 2018/7/27.
+ */
+public class TestUtil {
+    private static final String TAG = "testUtil";
+
+    public static void logE(String text) {
+        Log.e(TAG, text);
+    }
+}
